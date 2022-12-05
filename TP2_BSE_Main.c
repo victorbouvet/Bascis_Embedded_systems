b3be6cd58530c2c88a60930eb62c5a4c92c9c810
Victor <91113788+victorbouvet@users.noreply.github.com>
2022-12-05 17:44:22 +0100

    Update TP2_BSE_Main.c

--- TP2_BSE_Main.c
+++ TP2_BSE_Main.c
@@ -1,14 +1,14 @@
 //-----------------------------------------------------------------------------
 // TP2_BSE.c
 //-----------------------------------------------------------------------------
-// AUTH: 
+// AUTH: Victor Bouvet, Paul Moreau-Neidhardt
 // DATE: 
 //
 // Target: C8051F02x
 // Tool chain: KEIL Microvision5
 //
 //-----------------------------------------------------------------------------
-// Fichiers d'entête
+// Fichiers d'entÃªte
 #include "intrins.h"
 #include<c8051F020.h>
 #include<c8051F020_SFR16.h>
@@ -16,7 +16,7 @@
 #include<TP2_BSE_Lib_Divers.h>
 #include<TP2_BSE_Main.h>
 //-----------------------------------------------------------------------------
-// Déclaration des MACROS
+// DÃ©claration des MACROS
 #define LED_ON 1
 #define LED_OFF 0
 #define LED_BLINK 0
@@ -28,12 +28,12 @@
 #define RESET_VISU_DECL_EXTN P6 &= ~(1<<4)
 
 //-----------------------------------------------------------------------------
-// Déclarations Registres et Bits de l'espace SFR
+// DÃ©clarations Registres et Bits de l'espace SFR
 sbit LED = P1^6;  // LED
 sbit BP =P3^7;    // Bouton Poussoir 
 sbit DECL_EXTRN = P3^6; // Interruption externe
 //-----------------------------------------------------------------------------
-// Signaux témoins de l'oscilloscope
+// Signaux tÃ©moins de l'oscilloscope
 sbit VISU_BP = P2^4;
 bit VISU_DECL_EXTN;
 sbit Visu_INT_Timer2 = P3^5;
@@ -46,17 +46,17 @@ Event = PROCESSED;
 //Configuration des interruptions
 void Config_INT7(void)
 {
-	P3IF &= ~(1<<7); // IE7 mis à 0 pending flag de INT7 effacé
-	P3IF |= (0<<3); // IE7CF mis à 0 - sensibilité int7 front descendant	
+	P3IF &= ~(1<<7); // IE7 mis Ã  0 pending flag de INT7 effacÃ©
+	P3IF |= (0<<3); // IE7CF mis Ã  0 - sensibilitÃ© int7 front descendant	
 	
-	EIP2 &= ~(1<<5);  // PX7 mis à 0 - INT7 priorité basse
-	EIE2 |= (1<<5);  // EX7 mis à 1 - INT7 autorisée
+	EIP2 &= ~(1<<5);  // PX7 mis Ã  0 - INT7 prioritÃ© basse
+	EIE2 |= (1<<5);  // EX7 mis Ã  1 - INT7 autorisÃ©e
 }
 //******************************************************************************
 void ISR_INT7 (void) interrupt 19
 {
 	VISU_BP = 1;
-	P3IF &= ~(1<<7); // IE7 mis à 0 - remise à zéro du pending flag de INT7 effacé
+	P3IF &= ~(1<<7); // IE7 mis Ã  0 - remise Ã  zÃ©ro du pending flag de INT7 effacÃ©
 	Event = TO_BE_PROCESSED;
 	VISU_BP = 0;
 }	
@@ -64,11 +64,11 @@ void ISR_INT7 (void) interrupt 19
 //*****************************************************************************	 
 void Config_INT6(void)
 {
-	P3IF &= ~(1<<6); // DECL_EXTRN mis à 0 pending flag de INT6 effacé
-	P3IF |= (1<<2); // IE6CF mis à 1 - sensibilité int6 front montant
+	P3IF &= ~(1<<6); // DECL_EXTRN mis Ã  0 pending flag de INT6 effacÃ©
+	P3IF |= (1<<2); // IE6CF mis Ã  1 - sensibilitÃ© int6 front montant
 	
-	EIP2 &= ~(1<<4); // PX6 mis à 0 - INT6 priorité basse
-	EIE2 |= (1<<4); // EX6 mis à 1 - INT6 autorisée
+	EIP2 &= ~(1<<4); // PX6 mis Ã  0 - INT6 prioritÃ© basse
+	EIE2 |= (1<<4); // EX6 mis Ã  1 - INT6 autorisÃ©e
 }
 //*****************************************************************************	
 void ISR_INT6 (void) interrupt 18
@@ -76,38 +76,38 @@ void ISR_INT6 (void) interrupt 18
 	SET_VISU_DECL_EXTN;
 	
 	if((P3IF&(1<<2)) >0){
-		P3IF &= ~(1<<2); // IE6CF mis à 0 - sensibilité int6 front descendant
+		P3IF &= ~(1<<2); // IE6CF mis Ã  0 - sensibilitÃ© int6 front descendant
 	}
 	
 	else{
-	P3IF |= (1<<2); // IE6CF mis à 1 - sensibilité int6 front montant
+	P3IF |= (1<<2); // IE6CF mis Ã  1 - sensibilitÃ© int6 front montant
 	}
 	
-	P3IF &= ~(1<<6); // IE6 mis à 0 - remise à zéro du pending flag de INT6 effacé
+	P3IF &= ~(1<<6); // IE6 mis Ã  0 - remise Ã  zÃ©ro du pending flag de INT6 effacÃ©
 	Event = TO_BE_PROCESSED;	
 	RESET_VISU_DECL_EXTN;
 }
 //*****************************************************************************	
 void Config_TIMER2_BT(void)
 {
-	TR2 = 0; // désactiver le timer
+	TR2 = 0; // dÃ©sactiver le timer
 	
 	//CKCON &= 0x6f;
 	
 	TF2 = 0; // reset flag d'overflow
 	EXF2 = 0; // reset flag externe
-	CT2 = 0; // incrémentation par la clock T2M
+	CT2 = 0; // incrÃ©mentation par la clock T2M
 	CPRL2 = 0; // auto-reload
 	RCLK0 = 0; // auto-reload
 	TCLK0 = 0; // auto-reload
-	EXEN2 = 0; // dévalider entrée T2EX
+	EXEN2 = 0; // dÃ©valider entrÃ©e T2EX
 	
 	RCAP2L = 0x7E;
 	RCAP2H = 0xF9;
 	TL2 = RCAP2L;
 	TH2  = RCAP2H;
 	
-	PT2 = 1; // priorité haute
+	PT2 = 1; // prioritÃ© haute
 	ET2 = 1; // activer interruption
 	TR2 = 1; // activer le timer
 }
@@ -129,7 +129,7 @@ bit STATE_LED = LED_BLINK;
 
  	   // Configurations globales
 	      Init_Device();
-	   // Configurations  spécifiques  
+	   // Configurations  spÃ©cifiques  
 	      Config_INT7(); // Configuration de INT7
 				Config_INT6(); // Configuration de DECL_EXTRN
 				Config_TIMER2_BT(); // Configuration du Timer2
